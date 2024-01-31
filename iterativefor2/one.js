d7dbed981dde5865bdf3deb366263e116721dfe5
Nayanayadav2001 <nayanayadav2001@gmail.com>
2024-01-31 13:13:59 +0530

    js

---
+++ iterativefor2/one.js
@@ -0,0 +1,14 @@
+//for loop
+let i=0;
+for(i=0;i<=10;i++){
+    console.log(i)
+}
+
+//condition for forloop
+let a=1;
+for(;;){
+    if(a<=10){
+        console.log(a)
+    }
+    i++
+}
---
+++ iterativefor2/two.js
@@ -0,0 +1,19 @@
+//forloop
+for(let i=1; i<=10;i++){
+    console.log(i)
+}
+
+//whileloop
+i=10;
+while(i<=50){
+    console.log(i)
+    i++
+}
+
+//dowhileloop
+let a=10;
+do{
+    console.log(a)
+    a++
+}
+while(a<=10)
